1fb22bfaf90b7e275c9b9dce1140ad7c9b054420
Brock Taylor <13286385+brocktaylor7@users.noreply.github.com>
2022-06-15 13:28:23 -0700

    update 2022-06-15 13:28

--- dist/index.js
+++ dist/index.js
@@ -71907,21 +71907,18 @@ const gh_ioc_types_1 = __webpack_require__(/*! ./gh-ioc-types */ "./src/ioc/gh-i
 const check_run_creator_1 = __webpack_require__(/*! ../check-run/check-run-creator */ "./src/check-run/check-run-creator.ts");
 const gh_artifacts_info_provider_1 = __webpack_require__(/*! ../gh-artifacts-info-provider */ "./src/gh-artifacts-info-provider.ts");
 const console_comment_creator_1 = __webpack_require__(/*! ../console/console-comment-creator */ "./src/console/console-comment-creator.ts");
-const job_summary_creator_1 = __webpack_require__(/*! ../job-summary/job-summary-creator */ "./src/job-summary/job-summary-creator.ts");
 function setupIocContainer(container = new inversify.Container({ autoBindInjectable: true })) {
     container = (0, shared_1.setupSharedIocContainer)(container);
     container.bind(gh_ioc_types_1.GitHubIocTypes.Github).toConstantValue(github);
     container.bind(shared_1.iocTypes.TaskConfig).to(gh_task_config_1.GHTaskConfig).inSingletonScope();
     container.bind(check_run_creator_1.CheckRunCreator).toSelf().inSingletonScope();
-    container.bind(job_summary_creator_1.JobSummaryCreator).toSelf().inSingletonScope();
     container.bind(console_comment_creator_1.ConsoleCommentCreator).toSelf().inSingletonScope();
     container
         .bind(shared_1.iocTypes.ProgressReporters)
         .toDynamicValue((context) => {
         const consoleCommentCreator = context.container.get(console_comment_creator_1.ConsoleCommentCreator);
         const checkRunCreator = context.container.get(check_run_creator_1.CheckRunCreator);
-        const jobSummaryCreator = context.container.get(job_summary_creator_1.JobSummaryCreator);
-        return [checkRunCreator, consoleCommentCreator, jobSummaryCreator];
+        return [checkRunCreator, consoleCommentCreator];
     })
         .inSingletonScope();
     container
@@ -71935,83 +71932,6 @@ function setupIocContainer(container = new inversify.Container({ autoBindInjecta
     return container;
 }
 exports.setupIocContainer = setupIocContainer;
-
-
-/***/ }),
-
-/***/ "./src/job-summary/job-summary-creator.ts":
-/*!************************************************!*\
-  !*** ./src/job-summary/job-summary-creator.ts ***!
-  \************************************************/
-/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
-
-"use strict";
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var _a, _b, _c;
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.JobSummaryCreator = void 0;
-const inversify_1 = __webpack_require__(/*! inversify */ "../../node_modules/inversify/lib/inversify.js");
-const shared_1 = __webpack_require__(/*! @accessibility-insights-action/shared */ "../shared/dist/index.js");
-const gh_task_config_1 = __webpack_require__(/*! ../task-config/gh-task-config */ "./src/task-config/gh-task-config.ts");
-let JobSummaryCreator = class JobSummaryCreator extends shared_1.ProgressReporter {
-    constructor(taskConfig, reportMarkdownConvertor, logger) {
-        super();
-        this.taskConfig = taskConfig;
-        this.reportMarkdownConvertor = reportMarkdownConvertor;
-        this.logger = logger;
-        this.scanSucceeded = true;
-    }
-    start() {
-        this.logger.logDebug('job summary creator started');
-        return Promise.resolve();
-    }
-    completeRun(combinedReportResult) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const reportMarkdown = this.reportMarkdownConvertor.convert(combinedReportResult);
-            return yield this.taskConfig.writeJobSummary(reportMarkdown);
-        });
-    }
-    // eslint-disable-next-line @typescript-eslint/require-await
-    failRun() {
-        return __awaiter(this, void 0, void 0, function* () {
-            this.scanSucceeded = false;
-        });
-    }
-    didScanSucceed() {
-        return Promise.resolve(this.scanSucceeded);
-    }
-};
-JobSummaryCreator = __decorate([
-    (0, inversify_1.injectable)(),
-    __param(0, (0, inversify_1.inject)(shared_1.iocTypes.TaskConfig)),
-    __param(1, (0, inversify_1.inject)(shared_1.ReportMarkdownConvertor)),
-    __param(2, (0, inversify_1.inject)(shared_1.Logger)),
-    __metadata("design:paramtypes", [typeof (_a = typeof gh_task_config_1.GHTaskConfig !== "undefined" && gh_task_config_1.GHTaskConfig) === "function" ? _a : Object, typeof (_b = typeof shared_1.ReportMarkdownConvertor !== "undefined" && shared_1.ReportMarkdownConvertor) === "function" ? _b : Object, typeof (_c = typeof shared_1.Logger !== "undefined" && shared_1.Logger) === "function" ? _c : Object])
-], JobSummaryCreator);
-exports.JobSummaryCreator = JobSummaryCreator;
 
 
 /***/ }),
@@ -72197,15 +72117,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -72287,11 +72198,6 @@ let GHTaskConfig = class GHTaskConfig extends shared_1.TaskConfig {
             StaticSitePort: 'localhost-port',
         };
         return keyToName[key];
-    }
-    writeJobSummary(jobSummaryMarkdown) {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield this.actionCoreObj.summary.addRaw(jobSummaryMarkdown).write();
-        });
     }
     getUsageDocsUrl() {
         const url = 'https://github.com/microsoft/accessibility-insights-action/blob/main/docs/gh-action-usage.md';
